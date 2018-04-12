eb67a3f6ebf44497c828626266984a1d4bc27a17
严俊东 <1172482914@qq.com>
2018-04-12 14:33:40 +0800

    相关接口权限绑定

--- serve/router.js
+++ serve/router.js
@@ -13,7 +13,7 @@ const admin = common.admin
 router.get('*', common.init)
 
 // `用户`
-router.get('/user/search', common.search, user.search) // 搜索
+router.get('/user/search', admin, common.search, user.search) // 搜索
 router.post('/user/register', user.register) // 注册
 router.get('/user/session', user.getUser) // 获取当前登入用户
 router.get('/user/logout', user.logout) // 退出登入
@@ -21,15 +21,15 @@ router.post('/user/login', user.login) // 登入
 
 // `分类目录`
 router.get('/classify', classify.search) // 搜索
-router.post('/classify', classify.add) // 增加
-router.put('/classify', classify.modify) // 修改
-router.delete('/classify', classify.delete) // 删除
+router.post('/classify', admin, classify.add) // 增加
+router.put('/classify', admin, classify.modify) // 修改
+router.delete('/classify', admin, classify.delete) // 删除
 
 // 标签
 router.get('/tag', tag.search) // 搜索
-router.post('/tag', tag.add) // 增加
-router.put('/tag', tag.modify) // 修改
-router.delete('/tag', tag.delete) // 删除
+router.post('/tag', admin, tag.add) // 增加
+router.put('/tag', admin, tag.modify) // 修改
+router.delete('/tag', admin, tag.delete) // 删除
 
 // 绑定标签
 router.post('/place', admin, place.bind) // 绑定
