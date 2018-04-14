27acd88d3d505131cefd2f3b00f87d9c8e64cbc6
严俊东 <1172482914@qq.com>
2018-04-14 20:19:24 +0800

    `评论`参数校验

--- serve/class/Comment.js
+++ serve/class/Comment.js
@@ -4,34 +4,35 @@
 const error = require('../error')
 const common = require('./common')
 
-function Comment ({article_id, username, email, address, comment, father_id, show}) {
+function Comment ({article_id, username, email, address, comment, father_id}) {
     this.id
-    this.article_id // 文章ID
-    this.username // 用户名
-    this.email // 邮箱
-    this.address // 网址
-    this.comment // 评论内容
-    this.father_id // 父评论
-    this.create_time // 创建时间
-    this.show // 是否展示
+    this.article_id = article_id // 文章ID
+    this.username = this.checkLength(username, 10) // 用户名
+    this.email = this.checkEmail(email) // 邮箱
+    this.address = this.checkUrl(address) // 站点
+    this.comment = this.checkLength(comment, 150) // 评论内容
+    this.father_id = father_id // 父评论
 
+    this.show // 是否展示
+    this.create_time // 创建时间
     this.son_num // 子评论数量
 }
 
 /** 继承公共原型 */
 Comment.prototype = Object.create(common)
 
+/** 创建新对象 */
 Comment.prototype.create = function () {
-    this.checkNull()
+    this.checkNull(this.article_id, this.username, this.email, this.comment)
+    this.create_time = new Date()
+    this.son_num = 0
 }
 
-/** 用户名校验，非空，长度小于11 */
-Comment.prototype.checkUsername = function (username) {
-    if (!username || username.length > 10) {
-        throw error.usernameCheckFalse
-    } else {
-        return username
-    }
+/** 修改对象 */
+Comment.prototype.modify = function ({id, show}) {
+    this._delNull()
+    this.show = show
+    this.id = id
 }
 
 module.exports = Comment
--- serve/class/common.js
+++ serve/class/common.js
@@ -5,12 +5,39 @@
 const error = require('../error')
 
 module.exports = {
-    /** 非空校验 */
-    checkNull: function (val) {
-        if (!val) {
-            throw error.isNull
+    /** 是否为 null 或 undefined */
+    _isNull: function (val) {
+        return (val === null || val === undefined)
+    },
+    /** 删除 null 或 undefined 属性 */
+    _delNull: function () {
+        for (let key in this) {
+            if (this._isNull(this[key])) {
+                delete this[key]
+            }
+        }
+    },
+    /** 长度校验 */
+    checkLength: function (val, maxLength, minLength) {
+        if (this._isNull(val)) return null
+        minLength = minLength || 1
+        if (val.length > maxLength || val.length < minLength) {
+            throw error.lengthOverflow
         } else {
             return val
+        }
+    },
+    /** 非空校验 */
+    checkNull: function (val) {
+        if (!val instanceof Array) {
+            val = [val]
+        }
+        for (let i = 0; i < val.length; i++) {
+            if (this._isNull(val[i])) {
+                throw error.isNull
+            } else {
+                return val
+            }
         }
     },
     /** 空字符串校验 */
@@ -23,6 +50,7 @@ module.exports = {
     },
     // 邮箱校验
     checkEmail: function (email) {
+        if (this._isNull(email)) return null
         const regex = /^([0-9A-Za-z\-_\.]+)@([0-9a-z]+\.[a-z]{2,3}(\.[a-z]{2})?)$/g;
         if (!regex.test(email)) {
             throw error.emailCheckFalse
@@ -32,6 +60,7 @@ module.exports = {
     },
     // 网址校验
     checkUrl: function (url) {
+        if (this._isNull(url)) return null
         const regex = /^([hH][tT]{2}[pP]:\/\/|[hH][tT]{2}[pP][sS]:\/\/)(([A-Za-z0-9-~]+)\.)+([A-Za-z0-9-~\/])+$/;
         if (!regex.test(url)) {
             throw error.urlCheckFalse
